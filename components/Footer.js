d4d411ab38cd61a9ee533cc52150ce6c0c32cadd
mstanka <mice78.mm@gmail.com>
2021-06-21 20:31:02 +0200

    add Header and Footer

---
+++ components/Footer.js
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+import styles from '../styles/Footer.module.css';
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p>Copyright &copy; DJ Events 2021</p>
+      <p>
+        <Link href="./about">
+          <a>About This Project</a>
+        </Link>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
