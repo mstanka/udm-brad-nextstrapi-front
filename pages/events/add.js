5ad2b13ac8ca8c5e49ddb4f948b71f5f1d463952
mstanka <mice78.mm@gmail.com>
2021-06-21 20:18:11 +0200

    initial commit

---
+++ pages/events/add.js
@@ -0,0 +1,11 @@
+import Layout from '../../components/Layout';
+
+const AddEventPage = () => {
+  return (
+    <Layout>
+      <h1>Add new event</h1>
+    </Layout>
+  );
+};
+
+export default AddEventPage;
