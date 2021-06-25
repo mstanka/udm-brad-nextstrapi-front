4b421c2275d1e1f9692f44b186ca3c67ba57f045
mstanka <mice78.mm@gmail.com>
2021-06-25 10:51:54 +0200

    connect to Strapi API

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -25,13 +25,17 @@ const EventPage = ({ evt }) => {
         </div>
 
         <span>
-          {evt.date} at {evt.time}
+          {new Date(evt.date).toLocaleDateString('en-US')} at {evt.time}
         </span>
         <h1>{evt.name}</h1>
 
         {evt.image && (
           <div className={styles.image}>
-            <Image src={evt.image} width={960} height={600} />
+            <Image
+              src={evt.image.formats.medium.url}
+              width={960}
+              height={600}
+            />
           </div>
         )}
 
@@ -53,7 +57,7 @@ const EventPage = ({ evt }) => {
 export default EventPage;
 
 export async function getStaticPaths() {
-  const res = await fetch(`${API_URL}/api/events`);
+  const res = await fetch(`${API_URL}/events`);
   const events = await res.json();
 
   const paths = events.map((evt) => ({
@@ -67,7 +71,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params: { slug } }) {
-  const res = await fetch(`${API_URL}/api/events/${slug}`);
+  const res = await fetch(`${API_URL}/events?slug=${slug}`);
   const events = await res.json();
 
   return {
--- pages/index.js
+++ pages/index.js
@@ -25,10 +25,10 @@ const HomePage = ({ events }) => {
 export default HomePage;
 
 export async function getServerSideProps() {
-  const res = await fetch(`${API_URL}/api/events`);
+  const res = await fetch(`${API_URL}/events?_sort=date:ASC&_limit=3`);
   const events = await res.json();
 
   return {
-    props: { events: events.slice(0, 3) },
+    props: { events  },
   };
 }
